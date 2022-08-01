6374435cdaa9dfd8d5e0dc9af8c06ef8721e3d75
msubham193 <msubham193@gmail.com>
2022-08-01 10:49:10 +0530

    final commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");S
+const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
